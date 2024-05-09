660a8b42ab7bf1803dc0d7fcaa858a1c2fb1fc61
rafael169 <r.alvarado1693@gmail.com>
2024-05-09 07:43:26 -0500

    funciona

---
+++ index.js
@@ -0,0 +1,9 @@
+import app from "./app/app.js";
+import {mensajeConsola, menssa} from "./app/message/consola.js";
+
+
+
+app.listen(app.get("port"), ()=>{
+    mensajeConsola("puertoSuccess", `${menssa.puerto}  http://localhost:${app.get("port")}`);
+});
+
